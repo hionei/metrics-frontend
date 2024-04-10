9be1b66d28e3b8309f8c1f6fad73a6fda8658c54
hionei <egan.xdev@gmail.com>
2024-04-10 06:37:44 -0700

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -76,10 +76,9 @@ function App() {
         url="https://flareuniverse.xyz"
         keywords="Flare, Universe, Songbird, songbird, flare, ftso, oracle, system, SGB, FLR, monitor, metrics, assets, delegate, data providers,"
         description="This is a website (web3 dapp) that monitors ftso data providers's status supported by Flare Universe. And token holders can manage their assets easily including sending, wraping, delegating and auto-claiming etc. This is based on Songbird and Flare Blockchain Ecosystem"
-        image="https://raw.githubusercontent.com/flareuniverse/images/main/static/metaimg.jpg"
+        image="https://flareuniverse.vercel.app/metaimg.jpg"
       >
-        <meta name="og:country-name" content="United States" />
-        <meta property="og:image" content="https://raw.githubusercontent.com/flareuniverse/images/main/static/metaimg.jpg" />
+        <meta property="og:image" content="https://flareuniverse.vercel.app/metaimg.jpg" />
       </BetterHelmet>
       <RouterProvider router={router} />
     </>
