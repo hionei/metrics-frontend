d7cc03dd271447d142e21bd6aa836a827cd6d2ee
hionei <egan.xdev@gmail.com>
2024-04-10 13:38:16 -0700

    updated icon

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,7 @@ function App() {
         url="https://flareuniverse.xyz"
         keywords="Flare, Universe, Songbird, songbird, flare, ftso, oracle, system, SGB, FLR, monitor, metrics, assets, delegate, data providers,"
         description="This is a website (web3 dapp) that monitors ftso data providers's status supported by Flare Universe. And token holders can manage their assets easily including sending, wraping, delegating and auto-claiming etc. This is based on Songbird and Flare Blockchain Ecosystem"
+        favIcon="https://flareuniverse.xyz/favicon.png"
         // image="https://flareuniverse.vercel.app/metaimg.jpg"
       >
         {/* <meta property="og:image" content="https://flareuniverse.vercel.app/metaimg.jpg" /> */}
